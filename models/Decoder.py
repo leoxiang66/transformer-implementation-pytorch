98e5a7869c0ea0f4af97372d5284bb39a9b7421b
Tao Xiang <ge25pof@mytum.de>
2022-06-24 23:51:53 +0200

    update

---
+++ models/Decoder.py
@@ -0,0 +1,21 @@
+from put_everthing_together import *
+
+class Decoder(nn.Module):
+    def __init__(self,n_layers, hidden_dim) -> None:
+        super().__init__()
+        self.n_layers = n_layers
+        self.layers = nn.ModuleList([DecoderLayer(hidden_dim) for _ in range(n_layers)])
+    def forward(self,x1,x2,enc_att_mask,dec_att_mask):
+        '''
+
+        :param x1: decoder input: (N,L1,D)
+        :param x2: encoder output: (N,L2,D)
+        :param dec_att_mask: (N,L1)
+        :param enc_att_mask: (N,L2)
+        :return:   (N,L1,D)
+        '''
+
+        tmp = x1
+        for f in self.layers:
+            tmp = f.forward(tmp,x2,enc_att_mask,dec_att_mask)
+        return tmp
---
+++ models/Transformer.py
@@ -0,0 +1,21 @@
+from put_everthing_together import *
+class Transformer(nn.Module):
+    def __init__(self,n_layers,hidden_dim,n_labels) -> None:
+        super().__init__()
+        self.encoder = Encoder(n_layers,hidden_dim)
+        self.decoder = Decoder(n_layers,hidden_dim)
+        self.clf = ClassificationLayer(hidden_dim,n_labels)
+
+    def forward(self,enc_input, dec_input,enc_att_mask,dec_att_mask):
+        '''
+
+        :param enc_input: (N,L_enc,D)
+        :param dec_input: (N,L_dec,D)
+        :param enc_att_mask: (N,L_enc)
+        :param dec_att_mask: (N,L_dec)
+        :return: (N,L_dec,n_labels)
+        '''
+        enc_output = self.encoder(enc_input,enc_att_mask)
+        dec_output = self.decoder(dec_input,enc_output,dec_att_mask,enc_att_mask)
+        probs = self.clf(dec_output)
+        return probs
